e6d7dc3c94208f75e3754231beb66b8e5af87650
Mohd Anas <mohdanas@anas.local>
2024-02-10 13:48:14 +0530

    UI

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Switcher from './switcher';
+import Calculator from './calulator';
+
+function App() {
+  const [state, setState] = useState({
+    bill: 0,
+    people: 0,
+    tip: 0,
+    tipPerPerson: 0,
+    totalPerPerson: 0,
+  });
+
+  const calculateAmount = () => {
+    const { bill, tip, people } = state;
+
+    const totalAmount = bill + (bill * (tip / 100));
+
+    const tipPerPerson = totalAmount / people;
+    const totalPerPerson = totalAmount / people;
+
+    setState((prev) => ({ ...prev, tipPerPerson, totalPerPerson }));
+  };
+
+  useEffect(() => {
+    if(state?.bill && state?.tip && state?.people)
+    calculateAmount();
+  }, [state.bill, state.tip, state.people]);
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prev) => !prev);
+  };
+
+  return (
+ 
+
+    <div className="flex flex-col justify-center items-center w-full h-full overflow-y-auto bg-blue-100 p-10 ">
+        <div className='w-4/5 flex flex-col gap-21.25'>
+
+			<div className="flex w-full h-25  justify-center items-center">
+				<span className="flex font-IBMPlexMonoRegular font-semibold text-green-50 text-ft3">
+					SPLT
+					<br /> TTER
+				</span>
+			</div>
+			<div className="flex w-full justify-center items-center">
+				<Calculator />
+			</div>
+        </div>
+		</div>
+  );
+}
+
+export default App;
